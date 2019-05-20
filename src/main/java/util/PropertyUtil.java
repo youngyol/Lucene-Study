3e40954139da0e3043d70054903306a335ed91e5
youngyol <nasos9111@gmail.com>
2019-05-21 01:20:47 +0900

    Use properties file

---
+++ src/main/java/util/PropertyUtil.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyUtil {
+
+    public static final String PROPERTIES_SUFFIX = ".properties";
+    private final String propertyName;
+
+    private Properties props;
+
+    public PropertyUtil(String propertyName) {
+        this.propertyName = propertyName + PROPERTIES_SUFFIX;
+        this.readProperties();
+    }
+
+    private void readProperties() {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+
+        this.props = new Properties();
+        try (InputStream input = loader.getResourceAsStream(this.propertyName)) {
+            this.props.load(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getValue(String key) {
+        return this.props.getProperty(key);
+    }
+
+}
---
+++ src/test/java/util/PropertyUtilTest.java
@@ -0,0 +1,24 @@
+package util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class PropertyUtilTest {
+    private PropertyUtil propertyUtil;
+
+    @Before
+    public void setUp() {
+        this.propertyUtil = new PropertyUtil("test");
+    }
+
+    @Test
+    public void 프로퍼티파일_값_읽기(){
+        String expectedValue = "lucene";
+        String valueFromProperty = propertyUtil.getValue("test.value");
+
+        assertThat(expectedValue).isEqualTo(valueFromProperty);
+    }
+}
